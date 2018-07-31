7b80ca87325299ff384c937d77b91d60be0b9726
BossBaby <you@example.com>
2018-08-01 00:13:00 +0530

    Added moving enemy

--- New Unity Project/Assets/Scripts/Victory.cs
+++ New Unity Project/Assets/Scripts/Victory.cs
@@ -8,6 +8,7 @@ public class Victory : MonoBehaviour {
 
 	// if the player touches the victory object, it has not already been taken, and the player can move (not dead or victory)
 	// then the player has reached the victory point of the level
+
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if ((other.tag == "Player" ) && (!taken) && (other.gameObject.GetComponent<CharacterController2D>().playerCanMove))
